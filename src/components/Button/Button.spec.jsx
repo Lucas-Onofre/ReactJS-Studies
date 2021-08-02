72a4490e98c2d60b40c71c9abab951e5bc064b02
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2021-08-02 15:34:43 -0300

    react hooks final project

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,15 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { Button } from "."
+
+describe('<Button />', () => {
+    it('should render the button with the text "Load More"', () => {
+        render(<Button text="Load more" />)
+        expect.assertions(1)
+
+        const button = screen.getByRole('button', { name: /load more/i })
+
+        expect(button).toBeInTheDocument()
+    })
+})
+
